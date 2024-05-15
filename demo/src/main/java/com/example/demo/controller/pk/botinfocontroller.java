4edbad73a8f871cbf861b93bd1be0a83fb7a4cbb
wa1ess <wa1ess@outlook.com>
2024-05-16 01:03:22 +0800

    test1

--- demo/src/main/java/com/example/demo/controller/pk/botinfocontroller.java
+++ demo/src/main/java/com/example/demo/controller/pk/botinfocontroller.java
@@ -14,17 +14,11 @@ import java.util.Map;
 public class botinfocontroller {
 
     @RequestMapping("getbotinfo/")
-    public List<Map<String, String>> getbotinfo(){
-        List<Map<String, String>> list = new LinkedList<>();
+    public Map<String, String> getbotinfo(){
         Map<String ,String> bot1 = new HashMap<>();
         bot1.put("name", "tiger");
         bot1.put("rating", "1500");
-        Map<String ,String> bot2 = new HashMap<>();
-        bot2.put("name", "apple");
-        bot2.put("rating", "2500");
-        list.add(bot1);
-        list.add(bot2);
-        return list;
+        return bot1;
     }
 
 }
