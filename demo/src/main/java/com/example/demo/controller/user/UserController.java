4ea349284b546ddf1c450422a37792a1e3075319
wa1ess <wa1ess@outlook.com>
2024-05-18 03:46:30 +0800

    2024/05/18/3:44 a.m.

---
+++ demo/src/main/java/com/example/demo/controller/user/UserController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller.user;
+
+
+import com.example.demo.mapper.UserMapper;
+import com.example.demo.pojo.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+    @Autowired
+    UserMapper userMapper;
+
+
+    @GetMapping("/user/all/")
+    public List<User> getAll(){
+        return userMapper.selectList(null);
+    }
+
+
+}
